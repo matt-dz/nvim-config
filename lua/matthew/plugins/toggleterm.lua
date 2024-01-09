b63a03f750309940c15642f584cd003b565a6715
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-01-09 10:33:41 -0500

    Add event loading for toggleterm.nvim

--- lua/matthew/plugins/toggleterm.lua
+++ lua/matthew/plugins/toggleterm.lua
@@ -2,9 +2,13 @@ return {
 	-- amongst your other plugins
 	'akinsho/toggleterm.nvim',
 	version = "*",
-	lazy = false,
+	keys = {
+		{"<leader>tt"}
+	},
 	config = function()
 		require("toggleterm").setup({})
 		vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<cr>", { desc = "Opens a terminal in cwd" })
+		vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], { desc = "Escapes terminal mode" })
+		vim.keymap.set("t", "jk", [[<C-\><C-n>]], { desc = "Escapes terminal mode" })
 	end,
 }
