25ae6bb2f487717c3796994831cb585d64031e98
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-01-14 20:40:52 -0500

    Add zenbones.nvim

--- lua/matthew/core/init.lua
+++ lua/matthew/core/init.lua
@@ -4,3 +4,4 @@ vim.api.nvim_set_option('number', true)
 vim.api.nvim_set_option('relativenumber', true)
 vim.api.nvim_set_option('tabstop', 4)
 vim.api.nvim_set_option('shiftwidth', 4)
+vim.o.termguicolors = true
