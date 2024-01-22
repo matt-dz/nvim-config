9460d9c43815e4c27ddcb0fada552dd3ea4c14fd
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-01-22 13:37:39 -0500

    Add colorschemes

--- lua/matthew/lazy.lua
+++ lua/matthew/lazy.lua
@@ -14,4 +14,5 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	{ import = "matthew.plugins" },
 	{ import = "matthew.plugins.lsp" },
+	{ import = "matthew.plugins.colorscheme" },
 })
--- lua/matthew/plugins/colorscheme/zenbones.lua
+++ lua/matthew/plugins/colorscheme/zenbones.lua
@@ -1,7 +1,4 @@
 return {
 	"mcchrish/zenbones.nvim",
 	dependencies = { "rktjmp/lush.nvim" },
-	config = function()
-		vim.cmd('colorscheme zenbones')
-	end,
 }
