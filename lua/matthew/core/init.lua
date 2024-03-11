d4be8d83e2cb93a13fffc3b792fdfe048de9577b
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-03-10 21:37:49 -0400

    set cursorline true

--- lua/matthew/core/init.lua
+++ lua/matthew/core/init.lua
@@ -4,5 +4,6 @@ vim.api.nvim_set_option('number', true)
 vim.api.nvim_set_option('relativenumber', true)
 vim.api.nvim_set_option('tabstop', 4)
 vim.api.nvim_set_option('shiftwidth', 4)
+vim.api.nvim_set_option('cursorline', true)
 vim.opt.mouse = ""
 vim.o.termguicolors = true
