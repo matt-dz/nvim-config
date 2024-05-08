f56059b0b37baaac1172410716e4bf9396452e36
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-05-08 13:46:39 -0400

    Add autoformatter

--- lua/matthew/core/init.lua
+++ lua/matthew/core/init.lua
@@ -7,3 +7,12 @@ vim.api.nvim_set_option('shiftwidth', 4)
 vim.api.nvim_set_option('cursorline', true)
 vim.opt.mouse = ""
 vim.o.termguicolors = true
+
+-- Neoformat for format on save
+vim.api.nvim_create_augroup('fmt', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = 'fmt',
+  pattern = '*',
+  command = 'undojoin | Neoformat'
+})
+
