c4f46f02a836682989bcea118747d5a857c97625
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-05-13 22:58:19 -0400

    Change default colorscheme to kanagawa

--- lua/matthew/plugins/colorscheme/kanagawa.lua
+++ lua/matthew/plugins/colorscheme/kanagawa.lua
@@ -1,3 +1,9 @@
 return {
-	"rebelot/kanagawa.nvim"
+	"rebelot/kanagawa.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("kanagawa").load()
+		vim.cmd("colorscheme kanagawa")
+	end,
 }
--- lua/matthew/plugins/colorscheme/nordic.lua
+++ lua/matthew/plugins/colorscheme/nordic.lua
@@ -1,9 +1,4 @@
 return {
-	'AlexvZyl/nordic.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require 'nordic' .load()
-		vim.cmd('colorscheme nordic')
-    end
+	"AlexvZyl/nordic.nvim",
+	priority = 1000,
 }
