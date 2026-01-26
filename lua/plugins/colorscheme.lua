d2d95a11d6bcc5932fcae2587abf9a25f5e34a22
Matthew DeGuzman <matthew.deguzman@datadoghq.com>
2026-01-26 10:53:41 -0500

    feat: add cyberdream colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,13 @@
 return {
+	{ "catppuccin/nvim",          name = "catppuccin", priority = 1000 },
+	{ "ellisonleao/gruvbox.nvim", name = "gruvbox",    priority = 1000 },
+	{ "rebelot/kanagawa.nvim",    name = "kanagawa",   priority = 1000 },
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("catppuccin-mocha")
-		end
-	},
-	{ "ellisonleao/gruvbox.nvim", name = "gruvbox", priority = 1000 },
-	{ "rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000 },
+			vim.cmd.colorscheme("cyberdream")
+		end,
+	}
 }
